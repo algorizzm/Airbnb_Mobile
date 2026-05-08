f5c0f56b8bcd8f649e3ae10b293c64f9ebe672d3
jspdavis <jeddshendeldavis@gmail.com>
2026-05-09 01:07:45 +0800

    new jeddui

---
+++ write_profile.ps1
@@ -0,0 +1,5 @@
+﻿$xml = @'
+PLACEHOLDER
+'@
+[System.IO.File]::WriteAllText("c:\Users\Admin\Desktop\INTPROG\Hikora\app\src\main\res\layout\fragment_profile.xml", $xml, [System.Text.Encoding]::UTF8)
+Write-Host "Done"
